038e353a44f4ef1d55429498aca7b2cfb62e470f
Michał Filip Wysocki <micwys1995@gmail.com>
2020-10-25 20:55:18 +0100

    Update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { v4 as uuidv4} from 'uuid';
+
+const styles = {
+  backgroundColor: '#a4b0be',
+  border : '#aaa69d solid 2px',
+  color: '#dfe4ea',
+  margin: '18px auto',
+  padding: '12px 22px 10px',
+  textAlign: 'center',
+  width: 500,
+  height: 130,
+  title: {
+      color: '#ffffff'
+  },
+}
+
+const clickedHandler = () => {
+  window.location.reload();
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={styles}>
+      <h1 style={styles.title}>Your personal UUID4</h1>
+      <h2 style={styles.code}>{uuidv4()}</h2>
+      <button onClick={clickedHandler}>Refresh</button>
     </div>
   );
 }
